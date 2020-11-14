c89550ff8f574025cee4ec197db8bd93f84939e6
chriteli1 <christelioglanidis@outlook.com>
2020-11-14 16:59:49 +0200

    Error corrected askisi2_2

--- src/askisi2_2.cpp
+++ src/askisi2_2.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "ac_int.h"
 #include "ac_fixed.h"
-
+#include <stdlib.h>
+#include <time.h>
 //==============================================================
 
 template <int W>
 struct CSD {
- ac_int<W,false> x_p;
+ ac_int<W+1,false> x_p;
  ac_int<W,false> x_m;
 };
 
@@ -14,32 +15,14 @@ struct CSD {
 template <int W>
 void csd_encode (ac_int<W,true> &num, CSD<W> &num_csd){
 
-    // num is the input number to be encoded in CSD
-    // x_p and x_m are the two bit vectors that represent the CSD number
-    //ac_int<W,false> x_p;
-    //ac_int<W,false> x_m;
-
     int carry;
     carry = 0;
     bool flag = 0;
-    for(int i=0;i<W;++i) std::cout <<num[i] ;
-
-    //std::cout << "\n" << "1:" <<num << std::endl;
-
-    if(num < 0){
-        for(int k=0;k<W-1;++k){
-            if(num[k] == 1 && flag == 0) flag = 1;
-            else if(flag == 1) num[k] = num[k] + 1;
-            }
-        
-    }
-    //for(int i=0;i<W;++i) std::cout <<num[i] ;
-
-    //std::cout << "\n" << "2:" << num << std::endl;
+    
+    
     for(int i=0;i<W;++i){
 
-        
-        if(i == W-3){
+        if(i == W-1){
             if(carry == 1){
                 if(num[i] == 1){
                 num_csd.x_p[i] = 0;
@@ -63,7 +46,7 @@ void csd_encode (ac_int<W,true> &num, CSD<W> &num_csd){
             }
             
         }
-        else if(i < W-3){
+        else if(i < W-1){
 
             if(carry == 0){
                 if(num[i] == 0){
@@ -109,56 +92,85 @@ void csd_encode (ac_int<W,true> &num, CSD<W> &num_csd){
             num_csd.x_p[i] = 0;
             num_csd.x_m[i] = 0;
             }
-        //std::cout << "x_p:" << num_csd.x_p[i]  << std::endl;
+        
     }
     ac_int<W,true> CSD;
     CSD = num_csd.x_p - num_csd.x_m;
-    //std::cout << "x_p:" << num_csd.x_p  << std::endl;
-    //std::cout << "x_m:" << num_csd.x_m  << std::endl;
-    if(num < 0) CSD[W-1] = 1;
-    //std::cout<< "CSD:" <<std::endl;
-
+    std::cout << "x_p:" << num_csd.x_p  << std::endl;
+    std::cout << "x_m:" << num_csd.x_m  << std::endl;
+    std::cout << "Number after CSD:" << CSD << std::endl;
+    std::cout << "Number in Binary:";
     for(int i=0;i<W;++i){
         std::cout << CSD[i] ;
     }
-    //std::cout<< std::endl;
+    std::cout << std::endl;
 }
 //===================================================================
 
 
 
 template <int W>
-ac_int<W,true> csd_mult (ac_int<W,true> &in, CSD<W> &constant_csd){
- // in is the input value (non constant)
- // constant_csd is the CSD representation of a constant
+ac_int<2*W,true> csd_mult (ac_int<W,true> &in, CSD<W> &constant_csd){
     
-    CSD<W> num_csd;
-    //csd_encode(constant_csd,num_csd);
-    ac_int<W,true> y = 0;
+    ac_int<W,true> num_csd;
+    num_csd = constant_csd.x_p - constant_csd.x_m;
+    ac_int<2*W,true> y = 0;
+    ac_int<2*W,false> y2 = 0;
     ac_int<W,false> shifted_in; 
-    std::cout << "x_p:" << constant_csd.x_p  << std::endl;
-    std::cout << "x_m:" << constant_csd.x_m  << std::endl;
+    ac_int<W+1,false> twos_power1 = pow(2,W);
+    ac_int<2*W,false> twos_power2 = pow(2,2*W);
+    ac_int<W,true> in_temp = in;
+    ac_int<W,true> num_temp = num_csd;
 
-    for(int i=W-3;i>=0;--i){
+    
+    if(num_csd < 0){
+        num_csd = twos_power1 - num_csd;
+        num_csd[W-1] = 0;
+    }
+    
+    if(in < 0){
+        in = twos_power1 - in;
+        in[W-1] = 0;
+    }
+
+    
+
+    csd_encode(num_csd,constant_csd);
+    if(constant_csd.x_p[W] == 1){
+        shifted_in = in << W;
+        y = y + shifted_in ;
+    }
+    for(int i=W-1;i>=0;--i){
         if(constant_csd.x_m[i] == 1){
             
             shifted_in = in << i;
-            //std::cout << "heyo0" << in  << std::endl;
-            //std::cout << "heyo1" << shifted_in  << std::endl;
+            
             y = y - shifted_in ;
-            //std::cout << "heyo2" << shifted_in  << std::endl;
+            
             
         }
         else if(constant_csd.x_p[i] == 1){
             shifted_in = in << i;
-            //std::cout << "heyo3" << in  << std::endl;
-            //std::cout << "heyo4" << shifted_in  << std::endl;
             y = y + shifted_in ;
-            //std::cout << "heyo5" << shifted_in  << std::endl;
-            
+           
         }
+    }
+    
+    bool flag = 0;
+    if(in_temp[W-1] != num_temp[W-1]){
+        for(int i=0;i<2*W;++i){
+            if(y[i] == 1 && flag == 0)flag = 1;
+            else if(flag == 1)y[i] = y[i] -1;
+            
+        }        
         
     }
+        
+    
+    
+    
+    
+   
     return y;
 }
 
@@ -166,32 +178,28 @@ ac_int<W,true> csd_mult (ac_int<W,true> &in, CSD<W> &constant_csd){
 int main(){
     ac_int<10,true> number;
     ac_int<10,true> x;
-    ac_int<10,true> y;
-    CSD<10> csd_const;
+    ac_int<20,true> y;
+    
     CSD<10> num_csd;
-    number = 15;
+    srand (time(NULL));
+
+    ac_int<7,true> rand_num = rand();
+    number =  rand_num;
     x = -4;
-    //csd_const = 10;
+    std::cout << "Mult in:" << x << std::endl;
+    std::cout << "Number:" << number << std::endl;
     csd_encode(number,num_csd);
     y = csd_mult(x,num_csd);
-
+    
+    
+    std::cout << std::endl;
+    
     std::cout << "y:" << y << std::endl;
-    for(int i=0;i<10;++i){
+    std::cout << "y(in binary):";
+    for(int i=0;i<20;++i){
         std::cout << y[i];
     }
     std::cout << std::endl;
-
-
-    ac_int<6,false> test;
-    ac_int<6,false> shifted_test;
-    test = 5;
-    for(int i=0;i<4;++i){
-        shifted_test = test << i;
-    }
-    
-    std::cout << shifted_test << std::endl;
-
-
 
     return 0;
 }
