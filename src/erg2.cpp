ba31bcf57e3a26ee77f6aa3d9df831f8c1779848
chriteli1 <christelioglanidis@outlook.com>
2020-11-24 20:11:27 +0200

    Second version

--- src/erg2.cpp
+++ src/erg2.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "ac_int.h"
-
-void graph_color(ac_int<1,false> Adg_G[6][6]){
-    int color[6] = {0,0,0,0,0,0};
+class Graph{
+private:
+bool Adg_G[6][6];
+int color[6] = {0,0,0,0,0,0};
+int max;
+public:
+Graph(){};
+#pragma hls_design top
+int graph_color(bool Adg_G[6][6]){
     int c;
     for(int i=0;i<6;++i){
         c = 1;
@@ -17,22 +23,31 @@ void graph_color(ac_int<1,false> Adg_G[6][6]){
         color[i] = c;
         
     }
+    max = 0;
+    for(int m=0;m<6;++m){
+        if(color[m] > max){
+            max = color[m];
+        }
+    }
     std::cout << "Colors: ";
     for(int k=0;k<6;++k){
          std::cout << color[k];
     }
     std::cout << std::endl;
+    std::cout << "Number of colors needed: " << max << std::endl;
+    return max;
 }
-
+};
 int main() {
-    ac_int<1,false> Adg_G[6][6] =  {{0,1,0,0,1,1},
+    bool Adg_G[6][6] =  {{0,1,0,0,1,1},
                                     {1,0,1,0,0,1},
                                     {0,1,0,1,0,0},
                                     {0,0,1,0,1,0},
                                     {1,0,0,1,0,1},
                                     {1,1,0,0,1,0}
                                    };
-
-    graph_color(Adg_G);
+    int max;
+    Graph test1;
+    max = test1.graph_color(Adg_G);
 
 }
